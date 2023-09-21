7315606b2e2e00f16724b80c45fbb3f1826e87ed
Mat Sz <contact@matsz.dev>
2023-09-21 18:14:32 +0200

    add tests for bytes schemas

---
+++ __tests__/schemas/bytes.test.ts
@@ -0,0 +1,51 @@
+import { arrayBuffer, struct, uint16, uint8Array } from '../../src/index.js';
+
+describe('uint8Array', () => {
+  const testStruct = struct({
+    length: uint16(),
+    data: uint8Array(),
+  }).withByteLength('data', 'length');
+
+  it('should serialize correctly', () => {
+    expect(
+      testStruct.toByteArray({
+        data: new Uint8Array([0xff, 0xee, 0xdd, 0xcc]),
+      }),
+    ).toEqual(new Uint8Array([0x00, 0x04, 0xff, 0xee, 0xdd, 0xcc]));
+  });
+
+  it('should parse correctly', () => {
+    expect(
+      testStruct.fromByteArray(
+        new Uint8Array([0x00, 0x04, 0xff, 0xee, 0xdd, 0xcc]),
+      ),
+    ).toEqual({
+      data: new Uint8Array([0xff, 0xee, 0xdd, 0xcc]),
+    });
+  });
+});
+
+describe('arrayBuffer', () => {
+  const testStruct = struct({
+    length: uint16(),
+    data: arrayBuffer(),
+  }).withByteLength('data', 'length');
+
+  it('should serialize correctly', () => {
+    expect(
+      testStruct.toByteArray({
+        data: new Uint8Array([0xff, 0xee, 0xdd, 0xcc]).buffer,
+      }),
+    ).toEqual(new Uint8Array([0x00, 0x04, 0xff, 0xee, 0xdd, 0xcc]));
+  });
+
+  it('should parse correctly', () => {
+    expect(
+      testStruct.fromByteArray(
+        new Uint8Array([0x00, 0x04, 0xff, 0xee, 0xdd, 0xcc]),
+      ),
+    ).toEqual({
+      data: new Uint8Array([0xff, 0xee, 0xdd, 0xcc]).buffer,
+    });
+  });
+});
